c7d9e5c2c54af9d80a66398aceb87b9662e95bff
piopump <>
2020-07-01 02:31:01 +0300

    go rotate

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,23 @@ window.onload = function() {
     canvas.setAttribute('width', width);
     canvas.setAttribute('height', height);
 
+    var ball = {
+        positionX: 0,
+        positionY: 0,
+        positionZ: 0,
+        rotationX: 0,
+        rotationY: 0,
+        rotationZ: 0
+    }
+
+    var gui = new dat.GUI();
+    gui.add(ball, 'positionX').min(-5).max(5).step(0.1);
+    gui.add(ball, 'positionY').min(-5).max(5).step(0.1);
+    gui.add(ball, 'positionZ').min(-5).max(5).step(0.1);
+    gui.add(ball, 'rotationX').min(-0.2).max(0.2).step(0.001);
+    gui.add(ball, 'rotationY').min(-0.2).max(0.2).step(0.001);
+    gui.add(ball, 'rotationZ').min(-0.2).max(0.2).step(0.001);
+
     var renderer = new THREE.WebGLRenderer({canvas: canvas});
     renderer.setClearColor(0x000000);
 
@@ -18,10 +35,26 @@ window.onload = function() {
     scene.add(light);
 
     var geometry = new THREE.SphereGeometry(200, 12, 12);
-    var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+    var material = new THREE.MeshBasicMaterial({color: 0xffffff, vertexColors: THREE.FaceColors});
+
+    for (var i = 0; i < geometry.faces.length; i++) {
+        geometry.faces[i].color.setRGB(Math.random(), Math.random(), Math.random());
+    }
+
     var mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
-    renderer.render(scene, camera);
+    function loop() {
+        mesh.position.x += ball.positionX;
+        mesh.position.y += ball.positionY;
+        mesh.position.z += ball.positionZ;
+        mesh.rotation.x += ball.rotationY;
+        mesh.rotation.y += ball.rotationY;
+        mesh.rotation.y += ball.rotationZ;
+        
+        renderer.render(scene, camera);
+        requestAnimationFrame(function(){loop();});
+    }
+    loop();
 
 }
